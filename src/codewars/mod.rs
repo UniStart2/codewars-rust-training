3597bbf96a485e9c8d0907add25a55ed5998b22c
unistart <2604249649@qq.com>
2023-04-22 16:23:38 +0800

    修改文件位置

---
+++ src/codewars/mod.rs
@@ -0,0 +1,13 @@
+mod all_balanced_parentheses;
+mod array_diff;
+mod common_denominator;
+mod double_integer;
+mod find_next_square;
+mod going_to_zero_or_to_infinity;
+mod number_of_trailing_zeros_of_n;
+mod range_extraction;
+mod rectangle_into_square;
+mod reverse_words;
+mod string_with_end;
+mod unique_in_order;
+mod your_order_please;
--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,3 @@
-mod array_diff;
-mod unique_in_order;
-mod your_order_please;
-mod reverse_words;
-mod find_next_square;
-mod double_integer;
-mod string_with_end;
-mod rectangle_into_square;
-mod range_extraction;
-mod number_of_trailing_zeros_of_n;
-mod going_to_zero_or_to_infinity;
-mod common_denominator;
-mod all_balanced_parentheses;
+mod codewars;
 
 fn main() {}
